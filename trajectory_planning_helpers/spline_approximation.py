93584242053c325f3bce08fe756cb9ddabfb6a0f
Kurt Wilson <kurt4wilson@gmail.com>
2023-04-30 12:31:29 -0400

    scipy fix

--- trajectory_planning_helpers/spline_approximation.py
+++ trajectory_planning_helpers/spline_approximation.py
@@ -143,6 +143,8 @@ def spline_approximation(track: np.ndarray,
 # return distance from point p to a point on the spline at spline parameter t_glob
 def dist_to_p(t_glob: np.ndarray, path: list, p: np.ndarray):
     s = interpolate.splev(t_glob, path)
+    s = np.reshape(s, (2, ))
+
     return spatial.distance.euclidean(p, s)
 
 
